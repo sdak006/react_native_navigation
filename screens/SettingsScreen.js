2806c054b4c46401ba2f7306fd9bfaa993656163
Jade\Scott <sdak006@gmail.com>
2017-06-17 10:20:39 -0500

    Scaffolded screens

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class SettingsScreeen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>SettingsScreeen</Text>
+        <Text>SettingsScreeen</Text>
+        <Text>SettingsScreeen</Text>
+        <Text>SettingsScreeen</Text>
+        <Text>SettingsScreeen</Text>
+        <Text>SettingsScreeen</Text>
+      </View>
+    );
+  }
+}
+
+export default SettingsScreeen;
